88fc00caa677c9ba27853b014bb95addf69035fb
DuXawm22 <prawiravincent98@gmail.com>
2018-04-15 15:53:48 +0700

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,25 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  // logindetails: Login = {
-  //   username: "",
-  //   password: ""
-  // };
+   logindetails: Login = {
+     username: "",
+     password: ""
+   };
   constructor() { }
 
-  ngOnInit() {
-    this.heroForm = new FormGroup({
-        'name': new FormControl(this.hero.name, [
-          Validators.required,
-          Validators.minLength(4),
-          forbiddenNameValidator(/bob/i) // <-- Here's how you pass in the custom validator.
-        ]),
-        'alterEgo': new FormControl(this.hero.alterEgo),
-        'power': new FormControl(this.hero.power, Validators.required)
-      });
-
-  }
-  get name() { return this.heroForm.get('name'); }
-
-  get power() { return this.heroForm.get('power'); }
 }
